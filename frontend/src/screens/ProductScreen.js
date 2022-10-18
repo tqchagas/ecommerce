e6c53c716c8f10f8e75d29f7cce28f0db9d00517
Thiago Chagas <thiagochagas@MacBook-Air-de-Thiago.local>
2022-10-18 14:29:09 +1300

    products page

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,14 +1,47 @@
 import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-
-import Product from '../components/Product'
+import {Row, Col, Image, ListGroup, ListGroupItem, Button, Card} from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 import products from '../products'
 
+import { useParams } from "react-router-dom";
+
 function ProductScreen() {
+    let { id } = useParams(); 
+    const product = products.find((p) => p._id == id)
+
     return (
-        <div>
-            <h1>Product</h1>
-        </div>
+         <div>
+             <Link to='/' className='btn btn-light'>Go Back</Link>
+             <Row>
+                 <Col>
+                     <Image src={product.image} alt={product.name} />
+                 </Col>
+                 <Col md={3}>
+                    <ListGroup variant="flush"> 
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <h3>Price: ${product.price}</h3>
+                            {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Button 
+                                className="btn-block" type="button"
+                                disabled={product.countInStock == 0}
+                            >
+                                Add to cart
+                            </Button>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                 </Col>
+
+             </Row>
+             
+         </div>
     )
 }
 
